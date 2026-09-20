70f18c5c87ba104014fece028f78f2de9bead4d7
genn0710-max <genn0710@gmail.com>
2026-09-20 19:42:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// G検定学習システム Service Worker v3.2 (NetworkFirst & 完全オフライン対応)
-const CACHE_NAME = 'gkentei-v3.2';
+// G検定学習システム Service Worker v3.3 (Audio Flow 聞き流し機能追加 & キャッシュ自動更新)
+const CACHE_NAME = 'gkentei-v3.3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
